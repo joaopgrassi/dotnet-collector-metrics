5b23ae94c1a80ac4ffb388798245b210e8ce1bce
Joao Grassi <joao.grassi@dynatrace.com>
2021-11-16 11:28:37 +0100

    Add sample app
    Exporting metrics to a local running collector using the Dynatrace metrics exporter.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # dotnet-collector-metrics
+
+A simple console app demonstrating how to export metrics via a OpenTelemetry collector 
+using the [Dynatrace exporter](https://github.com/open-telemetry/opentelemetry-collector-contrib/tree/main/exporter/dynatraceexporter).
+
+## Requirements
+
+- .NET SDK 5.x
+- Docker/Compose
+
+## Running 
+
+Make sure to edit the `otel-collector-config.yaml` by adding your Dynatrace endpoint and api token. For more information see the export [README](https://github.com/open-telemetry/opentelemetry-collector-contrib/tree/main/exporter/dynatraceexporter).
+
+After modifying the config file, from the root folder:
+
+- Start the Collector container `docker-compose up`
+- Start the console app: `dotnet run --project .\src\App.csproj`
+
+The console app generates metrics and waits in intervals for the 
+export to take place.
---
+++ src/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using System;
+using System.Diagnostics.Metrics;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace Examples.Console
+{
+    public class Program
+    {
+        private static readonly Meter _appMeter = new("MyCompany.MyProduct.MyApp", "1.0");
+        private static readonly Counter<long> _myCounter = _appMeter.CreateCounter<long>("MyCounter");
+
+        public static async Task Main(string[] args)
+        {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.SetMinimumLevel(LogLevel.Debug)
+                        .AddConsole();
+            });
+            var logger = loggerFactory.CreateLogger<Program>();
+
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            using var provider = Sdk.CreateMeterProviderBuilder()
+                .AddMeter(_appMeter.Name)
+                .AddOtlpExporter().Build();
+            
+            logger.LogInformation("Running and emitting metrics...");
+            while (true)
+            {
+                _myCounter.Add(RandomNumberGenerator.GetInt32(1, 30), new("name", "apple"), new("color", "red"));
+                _myCounter.Add(RandomNumberGenerator.GetInt32(5, 50), new("name", "lemon"), new("color", "yellow"));
+
+                logger.LogInformation("Pausing for 2 seconds for export to happen");
+                await Task.Delay(2000);                
+            }
+        }
+    }
+}
